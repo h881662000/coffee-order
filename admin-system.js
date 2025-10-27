32898cf124b9ada90f9f7cea4e771d27b7b6b6eb
h881662000 <70651218+h881662000@users.noreply.github.com>
2025-10-27 15:53:05 +0800

    Add files via upload

--- admin-system.js
+++ admin-system.js
@@ -4,7 +4,7 @@ const AdminSystem = {
     credentials: {
         username: 'admin',
         // 密碼: admin123 (SHA-256 hash)
-        passwordHash: 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f'
+        passwordHash: '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9'
     },
 
     // 當前登入狀態
