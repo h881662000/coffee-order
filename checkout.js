5e5f383058cd370dd7c83586d19e554bdd80bbc9
h881662000 <70651218+h881662000@users.noreply.github.com>
2025-10-29 16:44:28 +0800

    Add files via upload

--- checkout.js
+++ checkout.js
@@ -224,7 +224,7 @@ function generateOrderNumber() {
 // 提交到 Google Sheets
 async function submitToGoogleSheets(orderData) {
     // Google Apps Script Web App URL
-    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTyVMzPWHAvqdmoxsdC7pxyz6kn-Aqsl9sZL49wZAz-gc-l4s2LZQWXFEcaVtdKAEo/exec';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxVZQbkAKm6KFlV2EuJh_SulQapYpSvTLP5IhOY-gYp86lh8UDcp_DbFEYiaHCS4MwY/exec';
 
     // 如果尚未設定 Google Apps Script URL，暫時儲存到 localStorage
     if (!GOOGLE_SCRIPT_URL || GOOGLE_SCRIPT_URL === 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL') {
